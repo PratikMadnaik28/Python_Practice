46a38d8db4d335a3b8dacb86b43e80c22b66e761
Pratik Madnaik <pratikmadnaik@gmail.com>
2025-04-28 18:15:33 -0400

    initial commit

---
+++ apna2.py
@@ -0,0 +1,10 @@
+a = float(input("Please enter number."))
+b = float(input("Please enter number."))
+c = float(input("Please enter number."))
+
+if a>b and a>c:
+    print("a is the biggest number from other two.")
+elif b>c:
+    print("b is the biggest number from other two.")
+else:
+    print("c is the biggest number from other two.")
